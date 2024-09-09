d288a0334cc9c4092b5728e27519d6b5a2e10d52
Kristófer Andersen <andersen19@gmail.com>
2024-09-09 12:46:42 +0200

    feat: setup

---
+++ OrkaNet.Entities/Trade.cs
@@ -0,0 +1,11 @@
+﻿namespace OrkaNet.Entities;
+public class Trade
+{
+    public int Id { get; set; }
+    public string TraderName { get; set; }
+    public string EnergyProduct { get; set; }
+    public decimal Quantity { get; set; }
+    public decimal Price { get; set; }
+    public DateTime TradeDate { get; set; }
+}
+
---
+++ OrkaNet/Controllers/TradesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrkaNet.Entities;
+
+namespace OrkaNet.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class TradesController : ControllerBase
+{
+    private static List<Trade> trades = new List<Trade>();
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Trade>> GetAllTrades()
+    {
+        return Ok(trades);
+    }
+
+    [HttpPost]
+    public ActionResult AddTrade([FromBody] Trade trade)
+    {
+        trades.Add(trade);
+        return Ok();
+    }
+}
